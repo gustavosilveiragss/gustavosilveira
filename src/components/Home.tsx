3de8816e3ef0aa8b72555d2c79280d8f0f9f3c4c
gustavosilveiragss <gustavo@pop-os.localdomain>
2023-02-26 19:32:00 -0300

    responsive design fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import test from "../../public/images/lakitu.jpg";
 const Home = () => (
     <div className="home hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-            <Image src={test} className="max-w-md rounded-lg shadow-2xl" alt=""></Image>
+            <Image src={test} className="lg:max-w-lg md:max-w-xl sm:max-w-sm rounded-lg shadow-2xl" alt=""></Image>
             <div>
                 <h1 className="text-5xl font-bold">Hey, I&apos;m Gustavo</h1>
                 <div className="w-fit px-2 border-solid border rounded-md text-secondary border-secondary mt-3">SOFTWARE DEVELOPER</div>
