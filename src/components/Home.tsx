6fd6cede9da3deefb6a261a56969d6f8c12646b5
gustavosilveiragss <gustavo@pop-os.localdomain>
2023-02-26 19:21:39 -0300

    change single quote to fix build

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => (
         <div className="hero-content flex-col lg:flex-row">
             <Image src={test} className="max-w-md rounded-lg shadow-2xl" alt=""></Image>
             <div>
-                <h1 className="text-5xl font-bold">Hey, I'm Gustavo</h1>
+                <h1 className="text-5xl font-bold">Hey, I&apos;m Gustavo</h1>
                 <div className="w-fit px-2 border-solid border rounded-md text-secondary border-secondary mt-3">SOFTWARE DEVELOPER</div>
-                <p className="py-6">I've got a passion for problem-solving and helping create new things</p>
+                <p className="py-6">I&apos;ve got a passion for problem-solving and helping create new things</p>
                 <a href="mailto:gustavo24gss@gmail.com">
                     <button className="btn btn-success">Contact</button>
                 </a>
